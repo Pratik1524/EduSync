0606c82a77feffb7fe756ecdebc875d0bd23e336
Pratik1524 <161496435+Pratik1524@users.noreply.github.com>
2025-09-25 12:26:00 +0530

    Add files via upload

---
+++ EduSync/server/server.js
@@ -0,0 +1,63 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import authRoutes from "./routes/authRoutes.js";
+import scheduleRoutes from "./routes/scheduleRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+// Get `__dirname` equivalent in ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname1 = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://atharva23092005.github.io/EduSync, http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+const MONGODB_URI="mongodb+srv://Gaurav5327:Gaurav5327@cluster0.prt6g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/schedule", scheduleRoutes);
+app.use("/api/users", userRoutes);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("availabilityUpdate", (data) => {
+    // Handle availability update
+    io.emit("scheduleUpdate", { type: "availability", data });
+  });
+
+  socket.on("preferenceUpdate", (data) => {
+    // Handle preference update
+    io.emit("scheduleUpdate", { type: "preference", data });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
